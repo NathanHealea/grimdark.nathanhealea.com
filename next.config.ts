319fae6a085d70fffbc6753e9686f6a732d1c119
Nathan Healea <nate.healea@gmail.com>
2025-06-27 09:29:25 -0700

    Update package.json and configuration files
    - Added "type": "module" to package.json
    - Updated scripts in package.json for prettier and database seeding
    - Added new dependencies: @heroicons/react, @supabase/ssr, @supabase/supabase-js, ts-node, daisyui, prettier, prettier-plugin-organize-imports, prettier-plugin-tailwindcss, and supabase
    - Updated devDependencies in package.json
    - Modified postcss.config.mjs to use single quotes and removed unnecessary newline
    - Enhanced tsconfig.json with additional path mappings for better module resolution

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  productionBrowserSourceMaps: true, // Only for production debugging, use with caution!
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Allows any hostname
+        // pathname: "**", // Allows any pathname (optional, and usually implied by hostname: "**")
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
