398d77fccbb923a520e245144b5cc1d9c8279f50
Nathan Healea <nate.healea@gmail.com>
2026-02-19 21:58:38 -0800

    Resolve issues with supabase/snippet directory

--- supabase/snippets/Untitled query 456.sql
+++
@@ -1,7 +0,0 @@
-  insert into public.user_roles (user_id, role_id)
-  select u.id, r.id
-  from auth.users u
-  cross join public.roles r
-  where u.email = 'llamanat3r@gmail.com'
-    and r.name in ('member')
-  on conflict do nothing;
